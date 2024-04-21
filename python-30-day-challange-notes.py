e84b63f6e6adcd3153e095d8aad1739e090221b1
Arsanyos <arsanyos1600@gmail.com>
2024-04-21 21:43:55 +0300

    rename-file, new notes

--- python-30-day-challange-notes.py
+++ python-30-day-challange-notes.py
@@ -1,10 +1,18 @@
 # python is an interpreted OOP language
 # use exit() to exist of python shell
 # 3//2 floor division 
-# 3%2 moduls devision (returns the reminder)
+# 3%2 modulo division (returns the reminder)
 
-#---#
+#-----------#
 
 # to write a string in python 'Arsanyos'
 # if the text is too long we use trpple qutoe to write a string ('''''')
    # (''' Lremnafjasf afkjshfksab sgjdkshgkdg skjahfaksf sakjdhfksadf sakjfhsakjf safkjhaskf sakfjhaskf''')
+
+#-----------#
+
+# Python indentation
+ """ in python space is used to create blocks of code.Whilst in other languages currly braces are used to create
+ that effect.
+  """
+ # AND YES WE USE THREE DOUBLE QUOTES AND WRAP AROUND THE CONTENT FOR A MULTILINE COMMENT
