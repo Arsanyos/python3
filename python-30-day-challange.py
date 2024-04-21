8562c28b0370f769e433c35b93cd02d93b8867a5
Arsanyos <arsanyos1600@gmail.com>
2024-04-21 21:30:28 +0300

    python-basics

---
+++ python-30-day-challange.py
@@ -0,0 +1,10 @@
+# python is an interpreted OOP language
+# use exit() to exist of python shell
+# 3//2 floor division 
+# 3%2 moduls devision (returns the reminder)
+
+#---#
+
+# to write a string in python 'Arsanyos'
+# if the text is too long we use trpple qutoe to write a string ('''''')
+   # (''' Lremnafjasf afkjshfksab sgjdkshgkdg skjahfaksf sakjdhfksadf sakjfhsakjf safkjhaskf sakfjhaskf''')
