087e8a09762f40e00b765110cb468e9b02c83ec8
Arsanyos <arsanyos1600@gmail.com>
2024-04-21 22:45:42 +0300

    complete exercises

---
+++ day-1/HelloWorld.py
@@ -0,0 +1,26 @@
+# 1 python --version
+
+# 2 
+
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+# 
+'Arsanyos'
+"Asrat"
+'Ethiopia'
+"I am enjoying 30 days of python"
+
+print(type(10))          # int
+print(type(9.8))        # float
+print(type(2.14))      # complex number
+print(type(4-4j))  # division (/)
+print(type(['Asabeneh', 'Python', 'Finland']))   # list
+print(type('Arsanyos'))    # dictionary 
+print(type('Asrat'))       # set
+print(type("Ethiopia"))       # tuple
--- day-1/python-30-day-challange-notes.py
+++ day-1/python-30-day-challange-notes.py
@@ -18,13 +18,7 @@
   """
  # AND YES WE USE THREE DOUBLE QUOTES AND WRAP AROUND THE CONTENT FOR A MULTILINE COMMENT
  
-print(2 + 3)             # addition(+)
-print(3 - 1)             # subtraction(-)
-print(2 * 3)             # multiplication(*)
-print(3 / 2)             # division(/)
-print(3 ** 2)            # exponential(**)
-print(3 % 2)             # modulus(%)
-print(3 // 2)            # Floor division operator(//)
+
 
 #-------------------------------------------------------#
                          # MY-ANSWERS
