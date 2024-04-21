55e391f332ae290dbeb9748aa27f90f8187a5087
Arsanyos <arsanyos1600@gmail.com>
2024-04-21 22:35:14 +0300

    euclidian distance

---
+++ EuclidianDistance.py
@@ -0,0 +1,8 @@
+import math
+
+coordinate_1 = (2,3)
+coordinate_2 = (10,8)
+
+euclidian_distance = math.sqrt((coordinate_2[1]-coordinate_1[1])**2 + (coordinate_2[0]-coordinate_2[0])**2)
+
+print('The Euclidian Distance is',euclidian_distance )
--- python-30-day-challange-notes.py
+++ python-30-day-challange-notes.py
@@ -2,7 +2,7 @@
 # use exit() to exist of python shell
 # 3//2 floor division 
 # 3%2 modulo division (returns the reminder)
-
+2 - 8
 #-----------#
 
 # to write a string in python 'Arsanyos'
@@ -12,7 +12,28 @@
 #-----------#
 
 # Python indentation
- """ in python space is used to create blocks of code.Whilst in other languages currly braces are used to create
+# 
+""" in python space is used to create blocks of code.Whilst in other languages currly braces are used to create
  that effect.
   """
  # AND YES WE USE THREE DOUBLE QUOTES AND WRAP AROUND THE CONTENT FOR A MULTILINE COMMENT
+ 
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+#-------------------------------------------------------#
+                         # MY-ANSWERS
+                         #-----------#
+print(type(10))          # int
+print(type(3.14))        # float
+print(type(1 + 3j))      # complex number
+print(type('Asabeneh'))  # division (/)
+print(type([1, 2, 3]))   # list
+print(type({'name':'Asabeneh'}))    # dictionary 
+print(type({9.8, 3.14, 2.7}))       # set
+print(type((9.8, 3.14, 2.7)))       # tuple
