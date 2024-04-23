f88f7868b92ff8a253cfad522dd54f61e83838d7
arsanyos <arsanyos1600@gmail.com>
2024-04-23 11:41:51 +0300

    completed day 2

---
+++ day-2/30DaysofPaythom.py
@@ -0,0 +1,19 @@
+# Built in functions in python
+# print() --- prints to the screen
+# len() --- counts the number of characters
+# type() --- returns the type
+# int() --- converts to string
+# float() --- converts to decimal point
+# input('Enter you xp level') --- accepts an input from the user
+
+
+#_------------------------------------------------------------------------#
+#A variable refers to a memory address in which data is stored.
+# When we assign a certain data type to a variable, it is called variable declaration
+
+first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+
+
+# role = input('What role do you have at your current company')
+print((zip([1,2],[3,4])))       
+# print(role)
---
+++ day-2/variables.py
@@ -0,0 +1,59 @@
+#'Day 2: 30 Days of python programming
+
+first_name = 'Arslan'
+middle_name = 'mano'
+last_name = 'bano'
+country ='Ethiopia'
+city="addis ababa"
+year="1999"
+is_married = False
+is_true = True
+is_light_on = False
+
+new_first_name , new_last_name = 'new', 'lastname'
+
+#2
+type(first_name)
+type(middle_name)
+type(last_name)
+
+type(country)
+type(city)
+type(year)
+
+type(is_married)
+type(is_true)
+type(is_light_on)
+
+
+len_first_name = len(first_name)
+len_last_name = len(last_name)
+
+
+if len_first_name > len_last_name:
+    print('fname is longer than lname')
+else:
+    print('lname is longer than fname')
+
+
+num_1 = 5
+num_2 = 4
+num_1_2_total = num_1 + num_2
+print(num_1_2_total)
+print( num_1 * num_2)
+print(num_1 / num_2)
+print(num_1**num_2)
+print(num_1 // num_2)
+
+
+radius_circle = 30
+
+area_of_circle  =2 * 3.14 * radius_circle**2
+circumference_of_circle = 2 * 3.14 * radius_circle
+
+
+user_radius = input('Radius input from user')
+user_radius_area_of_circle  =2 * 3.14 * user_radius**2
+
+
+
