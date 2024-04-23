ae205f34566d4678b9f7ce39a1d43a9725084f5b
Arsanyos <arsanyos1600@gmail.com>
2024-04-23 21:17:18 +0300

    bit wise operations,is,isnot,in,notin,menmonic variables

---
+++ day-3/30daysofpython.py
@@ -0,0 +1,11 @@
+x = 12
+name = 'arslan'
+
+# OR bit wise operator
+print(x|6)
+# AND bit wise operator
+
+print(name is 1) # False
+print('a' in name) # True
+print(name is not 'arslan') # False
+print('k' not in name) # True
