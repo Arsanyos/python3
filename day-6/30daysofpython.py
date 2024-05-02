acc68f8af9ae10170a29e61e0bb348d059f86de4
Arsanyos <arsanyos1600@gmail.com>
2024-05-03 01:31:05 +0300

    day 6 was about tuples

---
+++ day-6/30daysofpython.py
@@ -0,0 +1,21 @@
+#
+tpl = ('item 1','item 2','item 3')
+# there are only a few method when using tuples
+# tuples are ordered and unmodifiable
+# tuple() --- to create a new tuple
+# count() --- returns the instances of a certain item
+# index() --- returns the index of certain item
+
+# empty_tuple = ()
+# # OR #
+# empty_tuple = tuple()
+
+# tpl = ('item1', 'item2', 'item3')
+# print(len(tpl))
+
+# removing a certain item from a tuple is not possible however we can delete the whole tuple
+
+# tpl1 = ('Arsanyos',{'meoe':12},False)
+# del tpl1
+# print(tpl1)  ---- > retuns NameError because we have deleted tpl1 the python is asking if we meant tpl since no tuple with name tpl1 
+# exists anymore
